d90409a537b0648c1745df27acf856a0e319a872
AdamG95 <45023561+AdamG95@users.noreply.github.com>
2021-08-15 23:07:39 -0500

    movement improvements

--- car/car.cpp
+++ car/car.cpp
@@ -7,21 +7,25 @@ Car::Car(Motor front_right, Motor front_left, Motor back_right, Motor back_left,
     backRight = back_right;
     backLeft = back_left;
     ultrasonic = ultrasonic_sensor;
+    distance = 0;
 }
 
-
-uint64_t Car::getDistance()
+void Car::forward()
 {
-    uint8_t measurments = 5;
-    uint64_t sum;
+    frontRight.setDirection(-1);
+    backRight.setDirection(-1);
 
-    for (int i = 0; i < measurments; i++)
-    {
-        sum += ultrasonic.getDistance();
-        sleep_us(500);
-    }
+    frontLeft.setDirection(1);
+    backLeft.setDirection(1); 
+}
 
-    return sum / measurments;
+void Car::reverse()
+{
+    frontRight.setDirection(1);
+    backRight.setDirection(1);
+
+    frontLeft.setDirection(-1);
+    backLeft.setDirection(-1);
 }
 
 void Car::right()
@@ -40,28 +44,63 @@ void Car::left()
     backLeft.setDirection(-1);
 }
 
-void Car::forward()
-{
-    frontRight.setDirection(1);
-    backRight.setDirection(1);
-
-    frontLeft.setDirection(-1);
-    backLeft.setDirection(-1);
-}
-
-void Car::reverse()
-{
-    frontRight.setDirection(-1);
-    backRight.setDirection(-1);
-
-    frontLeft.setDirection(1);
-    backLeft.setDirection(1); 
-}
-
 void Car::stop()
 {
     frontRight.setDirection(0);
     frontLeft.setDirection(0);
     backRight.setDirection(0);
     backLeft.setDirection(0);
+}
+
+void Car::chooseDirection()
+{
+    const uint32_t turnTime = 350;
+    uint16_t multiplier = 1;
+    uint64_t distLeft;
+    uint64_t distRight;
+
+    while (true)
+    {
+        left();
+        sleep_ms(turnTime * multiplier);
+
+        stop();
+
+        distLeft = ultrasonic.getDistance();
+
+        right();
+        sleep_ms(turnTime * multiplier * 2);
+
+        stop();
+
+        if (distLeft > stopDistance && distLeft >= distRight)
+        {
+            left();
+            sleep_ms(turnTime * multiplier * 2);
+            break;
+        }
+        else if (distRight > stopDistance && distRight > distLeft)
+        {
+            break;
+        }
+        else
+            multiplier++;
+    }
+}
+
+void Car::drive()
+{
+    while(true)
+    {
+        distance = ultrasonic.getDistance();
+
+        if (distance > stopDistance)
+        {
+            forward();
+        }
+        else
+        {
+            chooseDirection();
+        }
+    }
 }
--- car/car.h
+++ car/car.h
@@ -9,21 +9,23 @@
 class Car
 {
     private:
+        const uint8_t stopDistance = 15;
         Motor frontRight;
         Motor frontLeft;
         Motor backRight;
         Motor backLeft;
         Ultrasonic ultrasonic;
         uint64_t distance;
-
-    public:
-        Car(Motor front_right, Motor front_left, Motor back_right, Motor back_left, Ultrasonic ultrasonic_sensor);
-        uint64_t getDistance();
         void forward();
         void reverse();
         void right();
         void left();
         void stop();
+        void chooseDirection();
+
+    public:
+        Car(Motor front_right, Motor front_left, Motor back_right, Motor back_left, Ultrasonic ultrasonic_sensor);
+        void drive();
 };
 
 #endif
