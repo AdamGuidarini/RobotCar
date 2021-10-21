5e0a3731e0039edfbedd07bacaecd259911ebb04
Adam Guidarini <45023561+AdamG95@users.noreply.github.com>
2021-10-21 01:11:13 -0500

    Add files via upload

---
+++ pico-car/main.cpp
@@ -0,0 +1,24 @@
+#include <cstdint>
+#include "pico/stdlib.h"
+#include "motor.h"
+#include "ultrasonic.h"
+#include "hardware/uart.h"
+#include "car.h"
+#include "drive.h"
+
+int main()
+{
+    sleep_ms(500);
+
+    Motor fLeft(16, 17);
+    Motor fRight(20, 21);
+    Motor bLeft(18, 19);
+    Motor bRight(22, 26);
+    Ultrasonic ultrasonic(5, 4);
+
+    Car car(fRight, fLeft, bRight, bLeft, ultrasonic);
+
+    car.drive();
+
+    return 0;
+}
